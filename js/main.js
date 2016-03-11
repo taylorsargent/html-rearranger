c493ee827b1e66a5125bb1f458c75e52159078bc
Taylor Sargent <sarg0049@algonquinlive.com>
2016-03-11 09:56:41 -0500

    added in jquery and rearranged

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var $h1Below = $('<h1>');
+var $ulBelow = $('<ul>');
+
+var $h1Above = $('<h1>');
+var $ulAbove = $('<ul>');
+
+$h1Below.html('Below ground veggies');
+
+$('body').append($h1Below);
+$('body').append($ulBelow);
+
+$h1Above.html('Above ground veggies');
+
+$('body').append($h1Above);
+$('body').append($ulAbove);
+
+$('ul li').each(function () {
+  if($(this).hasClass('below')){
+    $ulBelow.append($(this))
+  } else{
+    $ulAbove.append($(this))
+  }
+});
